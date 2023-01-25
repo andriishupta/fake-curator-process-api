091f2413f573e408c3a7ef0a7febe4bf54fc0e4c
Andrii Shupta <andrii.shupta@gmail.com>
2023-01-26 00:05:30 +0200

    build: switch to different build - renderer and flask

---
+++ index.py
@@ -0,0 +1,15 @@
+import spacy
+spacy.cli.download("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/api/process")
+def hello_world():
+    doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+    for token in doc:
+        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha,
+              token.is_stop)
+
+    return "<p>Hello, World!</p>"
